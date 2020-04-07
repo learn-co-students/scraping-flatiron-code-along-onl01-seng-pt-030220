ce057918dbecb05ec829eaec416617c2297447e9
cdominick091984 <cdominick091984@gmail.com>
2020-04-07 17:07:56 -0400

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,19 @@
+class Course
 
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def self.reset_all
+        @@all.clear
+    end
+    
+    attr_accessor :title, :schedule, :description
+
+    def initialize
+        @@all << self
+    end
+end
 
